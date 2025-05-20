566503838e29ba3458ac38b956ea961727505d4c
kabiriEl <elmahdikabiri2030@gmail.com>
2025-05-20 10:51:14 +0200

    j'ai changé la clé Api de 1.5 flash

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def ask_question(q: Question):
     response = get_answer(q.query, retriever)
     return {"answer": response}
 
-#test
+#testlkjhjkl
 
 # if __name__ == "__main__":
 #     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
